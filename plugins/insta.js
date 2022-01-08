6d4b76cf7f1c72fc3dbb452e6de50c4df69cb3cd
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-01-08 20:20:13 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -13,7 +13,7 @@ const s = require('../config');
 const v = s.CHANNEL
 const need = "_Need instagram link!_";
 const need_acc = "_Need an instagram username!_";
-const fail = "*Download failed! Check your link and try again*";
+const fail = "*_Download failed! Check your link and try again_*";
 const need_acc_s = "_Need username or link!_";
 let sourav = setting.WORKTYPE == 'public' ? false : true
 skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
@@ -33,17 +33,17 @@ var q = 'https://instagram.com' + souravkl11[1]
 var res = await raganork.query.getPost(q,v )
 var buffer = await raganork.query.skbuffer(res.links[0].url)
 if (!res) return await msg.client.sendMessage(msg.jid, fail, MessageType.text, {quoted: msg.data});
-if (res.links[0].url.includes('mp4')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.video, { mimetype: Mimetype.mp4,caption: '*ɪɴѕᴛᴀ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*' + '\n\n  𝗧𝗜𝗧𝗟𝗘:' + `${res.caption}` + '\n\n 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘: ' + `${res.username}` + , quoted: msg.data});
-if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '*ɪɴѕᴛᴀ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*' + '\n\n  𝗧𝗜𝗧𝗟𝗘:' + `${res.caption}` + '\n\n 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘: ' + `${res.username}` + , quoted: msg.data});
+if (res.links[0].url.includes('mp4')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.video, { mimetype: Mimetype.mp4, caption: '_Caption:_ ' + `${res.caption}` + '\n\n _Username:_ *' + `${res.username}` + '*\n _Name:_ *' + `${res.name}` + '*\n _Likes:_ *' + `${res.likes}` + '*\n _Comments:_ *' + `${res.comment_count}` + '*', quoted: msg.data});
+if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Caption:_ ' + `${res.caption}` + '\n\n _Username:_ *' + `${res.username}` + '*\n _Name:_ *' + `${res.name}` + '*\n _Likes:_ *' + `${res.likes}` + '*\n _Comments:_ *' + `${res.comment_count}` + '*', quoted: msg.data});
 }
 else return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
 }));
 skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
 if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc, MessageType.text, {quoted: msg.data});
 var res = await raganork.query.getStalk(query[1])
-var buffer = await raganork.query.skbuffer(res.user.hd_profile_pic_url_info)
+var buffer = await raganork.query.skbuffer(res.user.hd_profile_pic_url_info.url)
 if (!res.user) return await msg.client.sendMessage(msg.jid, "_Username invalid!_", MessageType.text, {quoted: msg.data})
-await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Name:_ ' + `${res.user.full_name}` + '\n _Bio:_ *' + `${res.user.biography}`+ '*\n _Private acc:_ *' + `${res.user.is_private} ` + '\n _Followers:_ *' + `${res.user.follower_count}` + '*\n _Following:_ *' + `${res.user.following_count}` + '*\n _Posts:_ *' + `${res.user.media_count}` + '*\n _Verified:_ *' + `${res.user.is_verified} ` + '*\n _IGTV videos:_ *' + `${res.user.total_igtv_videos}` + '*', quoted: msg.data});
+await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_Name:_ ' + `${res.user.full_name}` + '\n _Bio:_ *' + `${res.user.biography}`+ '*\n _Private acc:_ *' + `${res.user.is_private} ` + '*\n _Followers:_ *' + `${res.user.follower_count}` + '*\n _Following:_ *' + `${res.user.following_count}` + '*\n _Posts:_ *' + `${res.user.media_count}` + '*\n _Verified:_ ' + `${res.user.is_verified} ` + '\n _IGTV videos:_ *' + `${res.user.total_igtv_videos}` + '*', quoted: msg.data});
 }));
 skl.addCommand({ pattern: 'story ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
 if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc_s, MessageType.text, {quoted: msg.data});
