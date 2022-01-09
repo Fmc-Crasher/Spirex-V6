1ba0d7f0657e19ce1f88eb8c0c1dad95d0293809
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-01-09 09:19:37 +0530

    ᴄͥʀͭᴀᷤѕʜᷟᴇͤʀ

--- config.js
+++ config.js
@@ -82,11 +82,11 @@ module.exports = {
     DATABASE: DATABASE_URL === './whatsasena.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
     RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
     NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
-    SUDO: process.env.SUDO === undefined ? '919074309534,0' : process.env.SUDO,
+    SUDO: process.env.SUDO === undefined ? '919495944164,0' : process.env.SUDO,
     DEBUG: DEBUG,
     COFFEEHOUSE_API_KEY: process.env.COFFEEHOUSE_API_KEY === undefined ? false : process.env.COFFEEHOUSE_API_KEY,
     WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG",
     ZEKAIS_API: "1790e31da7e1d53197a23a0cdc679c5b",
-    SUPPORT2: "916282344739",
-    SUPPORT3: "919074309534"
+    SUPPORT2: "919495944164",
+    SUPPORT3: "918281657461"
 };
