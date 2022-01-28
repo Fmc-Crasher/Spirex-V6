5187f635f1f5faed68b984fdb54a3d86b7eb9a9a
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-01-28 22:00:18 +0530

    Update setlogo.js

--- plugins/setlogo.js
+++ plugins/setlogo.js
@@ -6,7 +6,7 @@ const config = require('../config');
 const Heroku = require('heroku-client');
 const heroku = new Heroku({token: config.HEROKU.API_KEY});
 let baseURI = '/apps/' + config.HEROKU.APP_NAME;
-New.addCommand({ pattern: 'logo ?(.*)', fromMe: true}, (async (message, match) => {
+New.addCommand({ pattern: 'setlogo ?(.*)', fromMe: true}, (async (message, match) => {
         if (match[1] == '') {return await message.sendMessage('_Need an image link!_')}
          if (match[1].includes('jpeg') || match[1].includes('raganork')) {
 var newimg = await axios.get(match[1], { responseType: 'arraybuffer' })
@@ -21,10 +21,10 @@ var newimg = await axios.get(match[1], { responseType: 'arraybuffer' })
         else return await message.sendMessage('_Image link invalid_ ❌ \n _Use command *.url* to get image link!_')
         }));
 
-New.addCommand({ pattern: 'mention ?(.*)', fromMe: true}, (async (message, match) => {
+New.addCommand({ pattern: 'mreply ?(.*)', fromMe: true}, (async (message, match) => {
         
-        if (!match[1]) {
-          return await message.sendMessage('Need a response! \n .mreply on \n or \n .mreply Message')
+        if (!match[1] && !message.reply_message) {
+          return await message.sendMessage('Need a response! \n .mreply on/off \n or \n .mreply Message \n or reply to any message')
         }
          else if (match[1] === 'on') {
          await heroku.patch(baseURI + '/config-vars', {
@@ -32,7 +32,7 @@ New.addCommand({ pattern: 'mention ?(.*)', fromMe: true}, (async (message, match
                 ['M_REPLY_VAR']: 'true'
             }
         });  
-         await message.sendMessage("_Mention reply turned on! Restarting.._")
+         await message.sendMessage("_Mention reply turned on! Restarting._")
          }
         else if (match[1] === 'off') {
          await heroku.patch(baseURI + '/config-vars', {
@@ -40,38 +40,25 @@ New.addCommand({ pattern: 'mention ?(.*)', fromMe: true}, (async (message, match
                 ['M_REPLY_VAR']: 'false'
             }
         });  
-         await message.sendMessage("_Mention reply turned off. Restarting.._")
+         await message.sendMessage("_Mention reply turned off. Restarting._")
          }
+         else if (message.reply_message.text) {
+            await heroku.patch(baseURI + '/config-vars', {
+               body: {
+                   ['M_REPLY']: message.reply_message.text
+               }
+           });  
+            await message.sendMessage("_New mention reply added. Restarting._")
+            }
           else {
             await heroku.patch(baseURI + '/config-vars', {
             body: {
                 ['M_REPLY']: match[1]
             }
         });
-        await message.sendMessage("_Added mention reply message! Restarting.._")
+        await message.sendMessage("_Added mention reply message! Restarting._")
           }
         
-    }));
-New.addCommand({ pattern: 'antifake ?(.*)', fromMe: true}, (async (message, match) => {
-        if (!match[1]) {
-          return await message.sendMessage('Need a response! \n .antifake on or off')
-        }
-         else if (match[1] === 'on') {
-         await heroku.patch(baseURI + '/config-vars', {
-            body: {
-                ['ANTI_FAKE']: 'true'
-            }
-        });  
-         await message.sendMessage("_Auto fake remove turned on! Restarting.._")
-         }
-        else if (match[1] === 'off') {
-         await heroku.patch(baseURI + '/config-vars', {
-            body: {
-                ['ANTI_FAKE']: 'false'
-            }
-        });  
-         await message.sendMessage("_Auto fake remove turned off. Restarting.._")
-         }
     }));
 
 New.addCommand({ pattern: 'mycode ?(.*)', fromMe: true}, (async (message, match) => {
