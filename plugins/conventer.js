a82fde3f08916968010d7255e7323d341f9d6471
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-01-28 23:00:39 +0530

    Update conventer.js

--- plugins/conventer.js
+++ plugins/conventer.js
@@ -1,22 +1,14 @@
-/*Codded by @phaticusthiccy
-Telegram: https://t.me/phaticusthiccy
-Instagram: https://instagram.com/kyrie.baran
-*/
-
 const Asena = require('../events');
 const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
-const {execFile} = require('child_process');
-const cwebp = require('cwebp-bin');
 const Config = require('../config');
 const cheerio = require('cheerio')
+const {query} = require('raganork-bot')
 const FormData = require('form-data')
 const Axios = require('axios');
-
 const Language = require('../language');
 const Lang = Language.getString('conventer');
-
 function webp2mp4File(path) {
     return new Promise(async (resolve, reject) => {
         const bodyForm = new FormData()
@@ -63,27 +55,6 @@ function webp2mp4File(path) {
     })
 }
 let sk = Config.WORKTYPE == 'public' ? false : true
-
-Asena.addCommand({pattern: 'mp3$', fromMe: sk, desc: Lang.MP4TOAUDİO_DESC}, (async (message, match) => {    
-        const mid = message.jid
-        if (message.reply_message === false) return await message.client.sendMessage(mid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text);
-        var downloading = await message.client.sendMessage(mid,Lang.MP4TOAUDİO,MessageType.text);
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        });
-
-        ffmpeg(location)
-            .save('output.mp3')
-            .on('end', async () => {
-                await message.client.sendMessage(mid, fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false});
-            });
-        return await message.client.deleteMessage(mid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
-    }));
-
     Asena.addCommand({pattern: 'photo$', fromMe: sk, desc: Lang.STİCKER_DESC}, (async (message, match) => {   
         const mid = message.jid
         if (message.reply_message === false) return await message.client.sendMessage(mid, Lang.STİCKER_NEEDREPLY, MessageType.text);
