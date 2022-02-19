ef5ba3453d46b073550d6204b55f8cb2d2a55e8e
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-02-19 19:26:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -87,10 +87,10 @@ module.exports = {
     DATABASE: DATABASE_URL === './whatsasena.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
     RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
     NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
-    SUDO: process.env.SUDO === undefined ? '919495944164,0' : process.env.SUDO,
+    SUDO: process.env.SUDO === undefined ? '919074309534,0' : process.env.SUDO,
     DEBUG: DEBUG,
     COFFEEHOUSE_API_KEY: process.env.COFFEEHOUSE_API_KEY === undefined ? false : process.env.COFFEEHOUSE_API_KEY,
     WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG",
-    SUPPORT2: "919495944164",
-    SUPPORT3: "919946493226"
+    SUPPORT2: "916282344739",
+    SUPPORT3: "919074309534"
 };
