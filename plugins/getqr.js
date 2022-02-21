d8cc7c2d28d3fdb35a2022692d3df46a2841d1d2
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-02-21 11:48:57 +0530

    Update getqr.js

--- plugins/getqr.js
+++ plugins/getqr.js
@@ -2,5 +2,5 @@
 var ra = require('../events');
 var {MessageType, Mimetype} = require('@adiwajshing/baileys');
 var axios = require('raganork-bot');
-ra.addCommand({pattern: 'getqr ?(.*)', fromMe: true, desc: 'Generates QR code for WhatsApp web' , dontAddCommandList: true }, async (m, q) => {
-await m.client.sendMessage(m.jid, await axios.query.skbuffer('https://baileys-qr.herokuapp.com/api/raganork-qr'), MessageType.image, {mimetype: Mimetype.jpg, quoted: m.data,caption:"1)You can only get session using this code \n 2) QR is only valid for 20 seconds"})});
+ra.addCommand({pattern: 'hermitqr ?(.*)', fromMe: true, desc: 'Generates QR code for WhatsApp web' , dontAddCommandList: true }, async (m, q) => {
+await m.client.sendMessage(m.jid, await axios.query.skbuffer('https://baileys-qr.herokuapp.com/api/raganork-qr'), MessageType.image, {mimetype: Mimetype.jpg, quoted: m.data,caption:"1)𝗚𝗢 𝗧𝗢 𝗟𝗜𝗡𝗞𝗘𝗗 𝗗𝗘𝗩𝗜𝗖𝗘 𝗢𝗙 𝗬𝗢𝗨𝗥 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣\n2) ѕᴄᴀɴ ғᴀѕᴛ ᴏɴʟʏ 𝟸𝟶 ѕᴇᴄ"})});
