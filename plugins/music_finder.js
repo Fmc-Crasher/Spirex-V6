78cb6fd0c6a1d76e8cc2ef13116560bc7974f679
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-02-21 12:29:50 +0530

    Update music_finder.js

--- plugins/music_finder.js
+++ plugins/music_finder.js
@@ -12,6 +12,6 @@ var r = await f.query.music(q,k,v)
 if (r.result) {
 let msg =  'ᴛɪᴛʟᴇ: *' + r.result.title + '* \nᴀʟʙᴜᴍ: *' + r.result.album+'* \nᴀʀᴛɪѕᴛ: *' + r.result.artist+ '* \nʟᴀʙᴇʟ: *' + r.result.label+'* \nʀᴇʟᴇѕᴇ ᴅᴀᴛᴇ: *' + r.result.release_date + '* \nѕᴏɴɢ ʟɪɴᴋ: ' + 'https://www.youtube.com/results?search_query='+r.result.title.split(' ').join('+')
 return await m.client.sendMessage(m.jid, msg, MessageType.text, {quoted: m.data})}
-if (r.error && r.error.error_code == '900') return await m.client.sendMessage(m.jid, "ᴀᴘɪ ᴋᴇʏ ᴇxᴘɪʀᴇᴅ ɢᴇᴛ ɪᴛ ᴏɴ https://audd.io", MessageType.text,{quoted: m.data})
+if (r.error && r.error.error_code == '900') return await m.client.sendMessage(m.jid, "ᴇʀᴏᴏʀ ᴀᴀʏɪ 😹", MessageType.text,{quoted: m.data})
 if (!r.result) return await m.client.sendMessage(m.jid, "ɪ ᴅᴏɴᴛ ᴋɴᴏᴡ ᴠʀᴏ", MessageType.text,{quoted: m.data})}
 else {return await m.client.sendMessage(m.jid, "ʀᴇᴘʟʏ 𝟸 ᴀᴜᴅɪᴏ", MessageType.text,{quoted: m.data})}}));
