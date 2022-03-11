eedeee34cfb2e3dd441325aad751f55c17e971b0
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-03-11 19:08:50 +0530

    Update truecaller.js

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -5,8 +5,8 @@ const {MessageType} = require('@adiwajshing/baileys');
 const v = s.SESSION
 const sourav = s.WORKTYPE == 'public' ? false : true
 New.addCommand({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',fromMe: sourav}, async (msg, query) => {
-if (!query[1] && !msg.reply_message) return await msg.reply("ɢɪᴠᴇ ᴍᴇ ᴀ ɴᴜᴍʙᴇʀ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜѕᴇʀ");
-if (query[1].includes('/')) return await msg.client.sendMessage(msg.jid, 'ᴡʀᴏɴɢ ғᴏʀᴍᴀᴛ! \n\n .true +91 6282071662', MessageType.text, {quoted: msg.data})
+if (!query[1] && !msg.reply_message) return await msg.reply("_Give me any number or reply to any user!_");
+if (query[1].includes('/')) return await msg.client.sendMessage(msg.jid, 'Wrong format! \n\n .true +91 6282344739', MessageType.text, {quoted: msg.data})
 var go;
 if (msg.reply_message) go = msg.reply_message.jid.split('@')[0]
 else if (!query[1].includes('@')) go = query[1]
@@ -20,5 +20,5 @@ go = mm
 var initt = go.split(" ").join("")
 var number = initt.replace('+','')
 const res = await truecaller.query.find(number,'',msg.client.user.jid,v)
-if (res == 'error') return await msg.sendMessage("ʟɪᴍɪᴛ ᴏᴠᴇʀ (20/20) ᴄᴏɴᴛᴀᴄᴛ ᴄʀᴀѕʜᴇʀ")
+if (res == 'error') return await msg.sendMessage("_Truecaller limit over! (20/20) Contact owner_")
 await msg.client.sendMessage(msg.jid, res, MessageType.text, {quoted: msg.data});});
