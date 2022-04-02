7956c9c5e6be9076aac521fe56713c739c3be748
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-04-02 10:58:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
-/* Copyright (c) Yusuf Usta & souravkl11
-Licenced under GNU
-Copying strictly proibited!
-*/
-
+// FILE: SOURAVKL11/RAGANORK
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
@@ -39,13 +35,16 @@ module.exports = {
     MUTEMSG: process.env.MUTE_MESSAGE === undefined ? 'default' : process.env.MUTE_MESSAGE,
     PLK: process.env.OWNER_NAME === undefined ? 'sᴏᴜʀᴀᴠᴋʟ¹¹' : process.env.OWNER_NAME,
     SONGD: process.env.SONGD === undefined ? '𝙎𝙚𝙖𝙧𝙘𝙝𝙞𝙣𝙜 𝙨𝙤𝙣𝙜... 🔍' : process.env.SONGD,
-    SONGU: process.env.SONGU === undefined ? '𝙎𝙤𝙣𝙜 𝙛𝙤𝙪𝙣𝙙! 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜... ⏫' : process.env.SONGU,
+    SONGU: process.env.SONGU === undefined ? '_<Uploading message here>_' : process.env.SONGU,
     BGMFILTER: process.env.BGM_FILTER === undefined ? false : convertToBool(process.env.BGM_FILTER),
     DISBGM: process.env.DISABLE_JID_BGM_FILTER === undefined ? false : process.env.DISABLE_JID_BGM_FILTER,
     AUTOSTICKER: process.env.AUTO_STICKER === undefined ? true : convertToBool(process.env.AUTO_STICKER),
+    warn_count: process.env.WARN === undefined ? '5' : process.env.WARN,
+    def_st_name: process.env.DF_STICKER_NAME === undefined ? 'false' : process.env.DF_STICKER_NAME,
     CODE: process.env.C_CODE === undefined ? '91' : process.env.C_CODE,
     DISSTICKER: process.env.DISABLE_STICKER === undefined ? false : process.env.DISABLE_STICKER,
     NOLOG: process.env.NO_LOG === undefined ? 'false' : process.env.NO_LOG,
+    AUTO_ACTION: process.env.AUTO_ACTION === undefined ? 'true' : process.env.AUTO_ACTION,
     CHATBOT: process.env.CHAT_BOT === undefined ? 'false' : process.env.CHAT_BOT,
     BOTSK: process.env.BOT_NAME === undefined ? 'PUBLIC BOT' : process.env.BOT_NAME,
     SUPPORT: process.env.ASENA === undefined ? '919074309534-1632403322,120363038867058369' : process.env.ASENA,
@@ -53,7 +52,7 @@ module.exports = {
     SLINK: process.env.IG === undefined ? 'instagram.com/sourav_kl11' : process.env.IG,
     JID: process.env.VERIFIED === undefined ? '0@s.whatsapp.net' : process.env.VERIFIED,
     GIF_BYE: process.env.GIF_BYE === undefined ? 'https://c.tenor.com/6tlIWiAlT_gAAAPo/cat-impressed-cat-wow.mp4' : process.env.GIF_BYE,
-    LOGOSK: process.env.ALL_IMG === undefined ? 'https://www.linkpicture.com/q/rgnk.jpg' : process.env.ALL_IMG,
+    LOGOSK: process.env.ALL_IMG === undefined ? 'https://www.linkpicture.com/q/rgnk.jpg' : process.env.ALL_IMG.split(',')[Math.floor(Math.random()*process.env.ALL_IMG.split(',').length)],
     created_by_souravkl11: process.env.ALL_IMG2 === undefined ? 'https://www.linkpicture.com/q/rgnk.jpg' : process.env.ALL_IMG2,
     take_key: process.env.TAKE_KEY === undefined ? '' : process.env.TAKE_KEY,
     find_key: process.env.FIND_KEY === undefined ? '' : process.env.FIND_KEY,
@@ -87,10 +86,12 @@ module.exports = {
     DATABASE: DATABASE_URL === './whatsasena.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
     RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
     NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
-    SUDO: process.env.SUDO === undefined ? '919495944164,0' : process.env.SUDO,
+    SUDO: process.env.SUDO === undefined ? '919074309534,0' : process.env.SUDO,
     DEBUG: DEBUG,
     COFFEEHOUSE_API_KEY: process.env.COFFEEHOUSE_API_KEY === undefined ? false : process.env.COFFEEHOUSE_API_KEY,
     WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG",
     SUPPORT2: "916282344739",
-    SUPPORT3: "919074309534"
+    SUPPORT3: "919074309534",
+    ST:"https://gist.github.com/souravkl11/",
+    RT:"4aa49c7848ba4e9224852576223e5de1/raw"
 };
