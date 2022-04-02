7012367d2d6b607979aa343edaa97938757a86b8
Fmc-Crasher <93718994+Fmc-Crasher@users.noreply.github.com>
2022-04-02 12:36:24 +0530

    Update music_finder.js

--- plugins/music_finder.js
+++ plugins/music_finder.js
@@ -1,17 +1,17 @@
 var e = require('../events')
 var {MessageType,Mimetype} = require('@adiwajshing/baileys')
 const c = require('../config')
-var f = require('raganork-bot')
+var {music} = require('raganork-bot')
 var v = c.SESSION
 var fm = c.WORKTYPE == 'public' ? false : true
 e.addCommand({pattern: 'find ?(.*)', fromMe: fm}, (async (m, match) => {    
 if (!m.reply_message.text && !m.reply_message.video && !m.reply_message.sticker && !m.reply_message.image) {
 var q = await m.client.downloadAndSaveMediaMessage({key: { remoteJid: m.reply_message.jid,id: m.reply_message.id}, message: m.reply_message.data.quotedMessage});
 var k = c.find_key
-var r = await f.query.music(q,k,v)
+var r = await music(q,k,v)
 if (r.result) {
-let msg =  'ᴛɪᴛʟᴇ: *' + r.result.title + '* \nᴀʟʙᴜᴍ: *' + r.result.album+'* \nᴀʀᴛɪѕᴛ: *' + r.result.artist+ '* \nʟᴀʙᴇʟ: *' + r.result.label+'* \nʀᴇʟᴇѕᴇ ᴅᴀᴛᴇ: *' + r.result.release_date + '* \nѕᴏɴɢ ʟɪɴᴋ: ' + 'https://www.youtube.com/results?search_query='+r.result.title.split(' ').join('+')
+let msg =  '_Title:_ *' + r.result.title + '* \n_Album:_ *' + r.result.album+'* \n_Artist:_ *' + r.result.artist+ '* \n_Label:_ *' + r.result.label+'* \n_Release date:_ *' + r.result.release_date + '* \n_Song link:_ ' + 'https://www.youtube.com/results?search_query='+r.result.title.split(' ').join('+')
 return await m.client.sendMessage(m.jid, msg, MessageType.text, {quoted: m.data})}
-if (r.error && r.error.error_code == '900') return await m.client.sendMessage(m.jid, "ᴇʀᴏᴏʀ ᴀᴀʏɪ 😹", MessageType.text,{quoted: m.data})
-if (!r.result) return await m.client.sendMessage(m.jid, "ɪ ᴅᴏɴᴛ ᴋɴᴏᴡ ᴠʀᴏ", MessageType.text,{quoted: m.data})}
-else {return await m.client.sendMessage(m.jid, "ʀᴇᴘʟʏ 𝟸 ᴀᴜᴅɪᴏ", MessageType.text,{quoted: m.data})}}));
+if (r.error && r.error.error_code == '900') return await m.client.sendMessage(m.jid, "_API Key expired! Get new api key from https://audd.io and setvar FIND_KEY:key_", MessageType.text,{quoted: m.data})
+if (!r.result) return await m.client.sendMessage(m.jid, "_Not found!_", MessageType.text,{quoted: m.data})}
+else {return await m.client.sendMessage(m.jid, "_Reply to any music!_", MessageType.text,{quoted: m.data})}}));
